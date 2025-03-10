dd3c662e3b52ce2fab1c15b2ea09922bed474f28
onyinye-paystack <onyinye@paystack.com>
2025-03-10 13:00:31 +0100

    beanstalk deployment test

---
+++ v3/.platform/hooks/postdeploy/01_datadog_stop.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export AWS_REGION=eu-west-1
+SSM_PARAM_NAME="datadog_started"
+LOG_FILE="/var/log/datadog_status.log"
+
+date | tee -a $LOG_FILE
+echo "Evaluating intended state of datadog..." | tee -a "$LOG_FILE"
+DATADOG_START=$(aws ssm get-parameter --name "$SSM_PARAM_NAME" --region $AWS_REGION  --query "Parameter.Value" --output text)
+echo "DATADOG_START is: $DATADOG_START" | tee -a "$LOG_FILE"
+
+if [ "$DATADOG_START" == "true" ]; then
+  echo "$(date) - SSM Parameter is true. Starting Datadog agent..." | tee -a "$LOG_FILE"
+  sudo docker start current-datadog-agent-1
+
+elif [ "$DATADOG_START" == "false" ]; then
+  echo "$(date) - SSM Parameter is false. Stopping Datadog agent..." | tee -a "$LOG_FILE"
+  sudo docker stop current-datadog-agent-1
+
+else
+  echo "$(date) - SSM Parameter is not recognized: $DATADOG_START" | tee -a "$LOG_FILE"
+fi
